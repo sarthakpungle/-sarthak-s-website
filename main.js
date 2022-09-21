61216a68a76199b7d123451c89b42e13687c5e58
sarthakpungle <111989180+sarthakpungle@users.noreply.github.com>
2022-09-21 17:59:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function changeImageOver() 
+{
+document.getElementById("profile").src="my.jpg";
+document.getElementById("profile")
+
+}
+
+function changeImageout()
+{
+    document.getElementById("profile").src="https://rizeyond.com/assets/images/thaer.png";
+}
+
+function ok(){
+    hi.style.color="cyan";
+    hi.style.border="5px solid indigo";
+    hi.style.borderRadius="50px";
+    document.getElementById("hi").innerHTML="Hi I am sarthak";
+}
+
+function ko(){
+    hi.style.color="yellow";
+    hi.style.border="0px"; 
+    document.getElementById("hi").innerHTML="Hi!";
+}
